faad602f15112556dae2c81dd86b666dc5f21b78
vega1030 <fvega.3010@gmail.com>
2023-05-01 19:50:16 +0200

    init tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,46 @@
-var assert = require('assert');
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+import { assert } from 'chai';
+import { Control_cart } from '../public/js/controller.js';
+
+describe('Control_cart', () => {
+  describe('control_Quantity', () => {
+    it('should return the total quantity of items in the cart', () => {
+      const controlCart = new Control_cart();
+
+      // Supongamos que esta es la data que pasaremos al método control_Quantity
+      const data = [
+        { id: 1, quantity: 2 },
+        { id: 2, quantity: 1 },
+        { id: 3, quantity: 4 },
+      ];
+
+      // Llamamos al método control_Quantity con la data de prueba
+      const result = controlCart.control_Quantity(data);
+
+      // Comprobamos que el resultado sea el esperado
+      assert.equal(result, 7);
+    });
+
+    it('should return 0 if cart is empty', () => {
+      const controlCart = new Control_cart();
+
+      // Llamamos al método control_Quantity sin pasar data
+      const result = controlCart.control_Quantity();
+
+      // Comprobamos que el resultado sea 0
+      assert.equal(result, 0);
+    });
+
+    it('should console.warn() and return undefined if data is falsy', () => {
+      const controlCart = new Control_cart();
+
+      // Llamamos al método control_Quantity con data falsy
+      const result = controlCart.control_Quantity(null);
+
+      // Comprobamos que el método haya llamado a console.warn()
+      assert.equal(console.warn.calledOnce, true);
+
+      // Comprobamos que el resultado sea undefined
+      assert.isUndefined(result);
     });
   });
 });
