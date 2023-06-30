52377b98d8f08e41cf9245b8e99318e660dced07
vega1030 <fvega.3010@gmail.com>
2023-06-30 13:06:22 +0200

    testing method quantity_In_Cart

--- test/storageMock.js
+++ test/storageMock.js
@@ -16,5 +16,4 @@ function localStorageMock() {
         },
     };
 }
-
 export{localStorageMock}
--- test/test.js
+++ test/test.js
@@ -1,74 +1,82 @@
-import { Control_cart } from '../public/js/controller.js';
 import { assert, expect } from 'chai';
 import { LocalStorage } from 'node-localstorage';
 import { createRequire } from 'module';
 import { JSDOM } from 'jsdom';
 import sinon from 'sinon';
-import { it } from 'mocha';
-
+import { it,describe } from 'mocha';
+import { Control_cart } from '../public/js/controller.js';
 const require = createRequire(import.meta.url)
 
-const test_LocalStorage = new LocalStorage('./scratch');
-global.test_LocalStorage = test_LocalStorage;
-const myModel = new Call_Api_LocalStorage_Cart(test_LocalStorage)
-
+const testLocalStorage = new LocalStorage('./scratch');
 const consoleWarnStub = sinon.stub(console, 'warn');
 afterEach(() => {
   consoleWarnStub.resetHistory();
 });
 
-const { window } =  JSDOM('<!DOCTYPE html>')
-global.window = window;
-global.document = window.document;
+const { document } = new JSDOM('<html><body></body></html>').window;
+globalThis.document = document;
+globalThis.window = document.defaultView;
 
+const testCart = new Control_cart()
 
-/* This code is testing the `quantity_In_Cart` method of the `Control_cart` class. It has three test
-cases: */
-describe('Control_cart', () => {
-  describe('control_Quantity', () => {
-    it('should return the total quantity of items in the cart', () => {
+describe('quantity_In_Cart', () => {
+  beforeEach(() => {
+    testLocalStorage.clear();
+  })
 
-      it('should return the total quantity of items in the cart', () => {
-        const controlCart = new Control_cart();
-        const data = [ { quantity: 100 }, { quantity: 900 }, { quantity: 23 }, ];
-        const result = controlCart.quantity_In_Cart(data);
-        assert.equal(result, 1023);
-      });
-    })
+  it.only('should return 0 if data is not provided', () => {
+    //Arrange
+    const data = undefined
+    //Act
+    
+    const result = testCart.quantity_In_Cart(data)
+    //Assert
+    expect(result).to.equal(0)
+  })
+  it.only('should calculate the total quantity and update localStorage', () => {
 
-    it('should return 0 if cart is empty', () => {
-      const controlCart = new Control_cart();
+    // Arrange
+    const data = [
+      { quantity: 2 },
+      { quantity: 3 },
+      { quantity: 1 },
+    ];
+    const expectedTotal = 6;
 
-      const result = controlCart.quantity_In_Cart();
+    // Act
+    const result = testCart.quantity_In_Cart(data);
+    // Assert
+    expect(result).to.equal(expectedTotal);
+    testLocalStorage.setItem('result',result)
 
-      assert.equal(result, 0);
-    });
+    expect(testLocalStorage.getItem('result')).to.equal('6')
 
-    it('should console.warn() and return undefined if data is falsy', () => {
-      const controlCart = new Control_cart();
-      beforeEach(() => {
+    // Add any additional assertions based on your implementation
+  })
+  it.only('should calculate the total quantity with one quantity is undefined and update localStorage', () => {
 
-        const consoleWarnStub = sinon.stub(console, 'warn');
+    // Arrange
+    const data = [
+      { quantity: 2 },
+      { quantity: 15 },
+      { quantity: 2 },
+      { quantity: 10 },
+      { quantity: 3 },
+      { quantity: undefined },
+    ];
+    const expectedTotal = 32;
 
-        const result = controlCart.quantity_In_Cart(null);
+    // Act
+    const result = testCart.quantity_In_Cart(data);
+    // Assert
+    expect(result).to.equal(expectedTotal);
+    testLocalStorage.setItem('result',result)
+    expect(testLocalStorage.getItem('result')).to.equal('32')
 
-        assert.isTrue(consoleWarnStub.calledOnce);
-        consoleWarnStub.restore();
-        assert.equal(consoleWarnStub.calledOnce, true);
-        assert.isUndefined(result);
-        console.log(result);
-      })
-      /* `afterEach` is a hook provided by the testing framework (Mocha in this case) that runs after each
-      test case. In this code, `afterEach` is used to restore the `console.warn` function to its
-      original state after each test case that stubs it using `sinon.stub(console, 'warn')`. This
-      ensures that the `console.warn` function is not permanently modified by the test cases and can be
-      used normally in other parts of the code. */
-      afterEach(() => {
-        console.warn.restore();
-      })
-    });
-  });
-});
+    // Add any additional assertions based on your implementation
+  })
+
+})
 
 //-------------------------//
 
