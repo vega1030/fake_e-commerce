c3bb4bb32093c92dc294c10be40d3b68282ebac1
vega1030 <fvega.3010@gmail.com>
2024-01-05 12:45:21 +0100

    separate localStorage of firebase realtime

---
+++ public/js/model/model.js
@@ -0,0 +1,89 @@
+
+'use strict'
+
+import { keysLocalStorage } from '../constants.js'
+/* It's a class that has a constructor that receives two parameters, and then has three methods that
+add, delete, and get products from local storage. */
+
+//These abstraction step at localStorage
+class StorageService {
+    setItem(key, value) {
+        const setValue = JSON.stringify(value)
+        if (typeof localStorage !== 'undefined') {
+            localStorage.setItem(key, setValue);
+        }
+    }
+
+    getItem(key) {
+        if (typeof localStorage !== 'undefined') {
+            const value = JSON.parse(localStorage.getItem(key)) || []
+            return value
+        }
+        return []
+    }
+
+    removeItem(key) {
+        localStorage.removeItem(key);
+    }
+}
+const local_Storage = new StorageService()
+
+//---------------------------------------/
+class Drive_Data_Cart {
+
+    constructor() {
+        this.modelCart
+
+    }
+
+
+    setAndCopyLocalStorage(cart) {
+        this.responseCart = cart
+        local_Storage.setItem(keysLocalStorage.CART, this.responseCart)
+        return this.responseCart
+    }
+    //***********--Cart--**************/ 
+
+
+    /* A function that receives an id as a parameter, gets the cart from local storage, subtracts the
+    product from the cart, and then saves the cart back to local storage. */
+
+
+    //*****************//***************** */
+
+
+}
+/***********--------------***************/
+const handler_Cart_Model = new Drive_Data_Cart()
+
+
+//***********--Favorites--**************/ 
+class Handler_Favorites {
+    constructor() {
+        this.favorites = []
+    }
+
+
+    saveFavorites(favorite){
+        this.favorites = favorite
+    }
+
+    sendFavorites(){
+        return this.favorites
+    }
+
+}
+
+handler_Cart_Model.setAndCopyLocalStorage(local_Storage.getItem(keysLocalStorage.CART))
+
+/***********--------------***************/
+
+
+
+
+export {
+
+    Drive_Data_Cart,
+    Handler_Favorites,
+    StorageService
+}
