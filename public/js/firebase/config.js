acdc65e73b6320d06c13e5f2f95fbf980b56ace9
vega1030 <fvega.3010@gmail.com>
2024-01-16 13:42:37 +0100

    restructuring files to class separates

--- public/js/firebase/config.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-export const firebaseConfig = {
-    apiKey: "AIzaSyB6C0ZD9j_WGUd0Wwrygb0EOKaYB7T6JME",
-    authDomain: "fakecommerce-2022.firebaseapp.com",
-    projectId: "fakecommerce-2022",
-    storageBucket: "fakecommerce-2022.appspot.com",
-    messagingSenderId: "568390621800",
-    appId: "1:568390621800:web:061d04c4c3ca57fab579dd"
-};
